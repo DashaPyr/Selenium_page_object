8809c0cbde0d9efcf87a5d74e14f1a6189414222
D Pyrkova <dpyrkova@unitarius.ru>
2023-07-25 11:29:02 +0300

    added pytest.ini and updeted test_main_page, test_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -3,30 +3,18 @@ from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 from .pages.base_page import BasePage
 import pytest
-import time
 import faker
 
 
-
-product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-
-list_of_failed_num = [7]
-tested_links = [f"{product_base_link}?promo=offer{i}" if i not in list_of_failed_num else
-                pytest.param(f"{product_base_link}?promo=offer{i}",
-                             marks=pytest.mark.xfail(reason="some bug", strict=True)
-                             )
-                for i in range(10)]
-link = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207?promo=offer0"]
-
-@pytest.mark.parametrize("link", link)
-def test_guest_can_add_product_to_basket(browser, link):
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207?promo=offer1"
     page = ProductPage(browser, link)
     page.open()
     page.press_button_add_to_basket()
     page.solve_quiz_and_get_code()
     page.basket_price_should_be_equal_product_price()
     page.book_name_same_in_basket_and_product_page()
-    # time.sleep(150)
 
 
 @pytest.mark.skip
@@ -56,7 +44,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.success_message_should_disappear()
 
 
-@pytest.mark.authorised_user
+@pytest.mark.registered_user
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
@@ -69,7 +57,7 @@ class TestUserAddToBasketFromProductPage():
         email = faker.Faker().email()
         password = faker.Faker().password()
         login_page.register_new_user(email, password)
-        browser.implicitly_wait(10)
+        browser.implicitly_wait(30)
         new_page = BasePage(browser, browser.current_url)
         new_page.should_be_authorized_user()
     
@@ -80,7 +68,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
     
-    
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207?promo=offer0"
         page = ProductPage(browser, link)
@@ -97,18 +85,18 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
     
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_basket()
